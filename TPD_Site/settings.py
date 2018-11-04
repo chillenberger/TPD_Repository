83a66ff8057ddee1fcfdd0d745a980ff843ca242
chillenberger <39170265+chillenberger@users.noreply.github.com>
2018-11-03 20:38:58 -0400

    change upwork icon css for mobile and test allowed host

--- TPD_Site/settings.py
+++ TPD_Site/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['tinyplanetdesign.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 # Application definition
 
